81857aa2b81e4ea4099653e702cf077fe8c33e79
Muhammad Junaid <junaid.paracha2010@gmail.com>
2019-02-14 22:25:09 +0500

    makes some changes in database.

--- FYP_project_database.sql
+++ FYP_project_database.sql
@@ -1,14 +1,16 @@
 create database OnlineClinic
 create table Doctor(
-Doc_Id int not null identity(1,1),
+Doc_Id int not null identity(1,1) primary key,
 Doc_Name varchar(30),
-Doc_Specialist varchar(30),
+Doc_Speciality varchar(30),
 Doc_Cnic int,
 Doc_Contact# int,
+
+/*constraint pk_Doc_Id primary key(Doc_Id),*/
 )
 
 create table Patient(
-Patient_Id int not null identity(1,1),
+Patient_Id int not null identity(1,1) primary key,
 Patient_Name varchar(30),
 Patient_Cnic int,
 Patient_BloodGroup varchar(30),
@@ -18,31 +20,39 @@ Patient_Adress varchar(50),
 Patient_Contact# int,
 )
 
+create table Gender(
+Gender_Id int not null identity(1,1) primary key,
+Male varchar(30),
+Female varchar(30),
+)
+
 create table Heart(
-Heart_Id int not null identity(1,1),
+Heart_Id int not null identity(1,1) primary key,
 Heart_Rate varchar(50),
-Start_Time varchar(50),
-End_Time varchar(50),
+[Time] time,
 [Date] date,
 )
 
 create table Physical_Activity(
-Activity_Id int not null identity(1,1),
-Activity_Date Date(),
+Activity_Id int not null identity(1,1) primary key,
+Activity_Date date,
 Calories varchar(30),
-Activity_Timespend varchar(30),
-Acitivity_Type varchar(30),
+StartTime varchar(30),
+EndTime varchar(30),
+)
+
+create table Activity_Type(
+Activity_TypeId int not null identity(1,1) primary key,
+Running varchar(30),
+walking varchar(30),
 )
 
 
 create table Nutrition(
-Food_Id int not null identity(1,1),
+Food_Id int not null identity(1,1) primary key,
 Food_Name varchar(50),
 Food_Calories varchar(30),
 Food_Protien varchar(30),
 Food_Carbohydrates varchar(30),
 Food_Cholestrol varchar(30)
 )
-
-
-create table Appointment()
