828f4119c4dfc2165ac216c71ccdda07d49b95ef
Muhammad Junaid <junaid.paracha2010@gmail.com>
2019-02-15 22:04:04 +0500

    Making changes in FYP_DATABASE

--- FYP_project_database.sql
+++ FYP_project_database.sql
@@ -1,58 +1,103 @@
 create database OnlineClinic
+
 create table Doctor(
 Doc_Id int not null identity(1,1) primary key,
-Doc_Name varchar(30),
-Doc_Speciality varchar(30),
-Doc_Cnic int,
+Doc_Name nvarchar(30) not null,
+Doc_Specialty nvarchar(30),
+Doc_CNIC int,
 Doc_Contact# int,
-
+Activity_Id int,
+Food_Id int,
+Heart_Id int,
 /*constraint pk_Doc_Id primary key(Doc_Id),*/
 )
+Alter table Doctor add constraint Doctor_Activity_Id_FK
+Foreign Key (Activity_Id) references Physical_Activity(Activity_Id)
+
+Alter table Doctor add constraint Doctor_Food_Id_FK
+Foreign Key (Food_Id) references Nutrition(Food_Id)
+
+Alter table Doctor add constraint Doctor_Heart_Id_FK
+Foreign Key (Heart_Id) references Heart(Heart_Id)
+
+
 
 create table Patient(
 Patient_Id int not null identity(1,1) primary key,
-Patient_Name varchar(30),
-Patient_Cnic int,
-Patient_BloodGroup varchar(30),
-Patient_DOB varchar(30),
-Patient_Sex varchar(20),
-Patient_Adress varchar(50),
+Patient_Name nvarchar(30) not null,
+Patient_CNIC int,
+Patient_BloodGroup nvarchar(30),
+Patient_Age nvarchar(30),
+Patient_Adress nvarchar(50),
 Patient_Contact# int,
+Doc_Id int,
+Gender_Id int,
+Activity_Id int,
+Food_Id int, 
+Heart_Id int,
 )
+
+Alter table Patient add constraint Patient_Doc_Id_FK
+Foreign Key (Doc_Id) references Doctor(Doc_Id)
+
+Alter table Patient add constraint Patient_Gender_Id_FK
+Foreign Key (Gender_Id) references Gender(Gender_Id)
+
+Alter table Patient add constraint Patient_Activity_Id_FK
+Foreign Key (Activity_Id) references Physical_Activity(Activity_Id)
+
+Alter table Patient add constraint Patient_Food_Id_FK
+Foreign Key (Food_Id) references Nutrition(Food_Id)
+
+
+
 
 create table Gender(
 Gender_Id int not null identity(1,1) primary key,
-Male varchar(30),
-Female varchar(30),
+Male nvarchar(30),
+Female nvarchar(30),
+Other nvarchar(30),
 )
 
 create table Heart(
 Heart_Id int not null identity(1,1) primary key,
-Heart_Rate varchar(50),
+Heart_Rate nvarchar(50),
 [Time] time,
 [Date] date,
 )
 
 create table Physical_Activity(
 Activity_Id int not null identity(1,1) primary key,
+Activity_TypeId int,/*Foreign key*/
 Activity_Date date,
-Calories varchar(30),
-StartTime varchar(30),
-EndTime varchar(30),
+Calories nvarchar(30),
+StartTime nvarchar(30),
+EndTime nvarchar(30),
 )
+
+Alter table Physical_Activity add constraint Patient_Activity_Activity_TypeId_FK
+Foreign Key (Activity_TypeId) references Activity_Type(Activity_TypeId)
 
 create table Activity_Type(
 Activity_TypeId int not null identity(1,1) primary key,
-Running varchar(30),
-walking varchar(30),
+Running nvarchar(30),
+walking nvarchar(30),
 )
 
 
 create table Nutrition(
 Food_Id int not null identity(1,1) primary key,
-Food_Name varchar(50),
-Food_Calories varchar(30),
-Food_Protien varchar(30),
-Food_Carbohydrates varchar(30),
-Food_Cholestrol varchar(30)
+Food_Name nvarchar(50) not null,
+Food_Calories nvarchar(30),
+Food_Protien nvarchar(30),
+Food_Carbohydrates nvarchar(30),
+Food_Cholestrol nvarchar(30)
 )
+
+select * from Doctor
+select * from Patient
+select * from Gender
+select * from Heart
+select * from Physical_Activity
+select * from Activity_Type
+select * from Nutrition
